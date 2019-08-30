9b8d8d635b639ecf9cf7123e06922e82c4c4340a
lethanh9398 <lethanh9398@gmail.com>
2019-08-30 13:59:02 +0700

    example docker

---
+++ src/main/java/com/lethanh98/Main.java
@@ -0,0 +1,19 @@
+package com.lethanh98;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * author lethanh9398 <p>
+ * create Date 30-08-2019 13:53  <p>
+ * document : <p>
+ * <p>- vi :
+ * <p>- en :
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+    }
+}
